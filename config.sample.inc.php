6b07b3a0c28218802e2dfd218f1651ca78416300
marhei <marhei@mac.com>
2012-12-28 21:33:36 +0100

    Unnötige Dateien entfernt, Standard-Config hinzugefügt.

---
+++ config.sample.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* Konfiguration für diese Installation von TrainCompany
+* Generiert am: 28.12.2012
+*
+**/
+
+namespace Config {
+	const INSTALLED = false;
+	const INSTALL_TIME = 0;
+	const DEBUG = true;
+	const TIME_ZONE = 'Europe/Berlin';
+}
+
+namespace Config\Version {
+	const STRING = '0.7.0';
+	const LICENSE_FILE = '/docs/license.txt';
+}
+
+namespace Config\MySQL {
+	const SERVER = NULL;
+	const USER = NULL;
+	const PASS = NULL;
+	const DATABASE = 'traincompany';
+}
+
+?>
